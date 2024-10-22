abfbb46e7267682d8c95674601046c802256e2ee
AMIR1606 <amersheikhhader@gmail.com>
2024-10-22 21:32:24 +0300

    new project

---
+++ WorkOrHole.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+  public class WorkOrHole {
+
+    public static void main(String [] args) {
+
+      Scanner input = new Scanner(System.in);
+          System.out.println("Gunu gir: ");
+      String day = input.nextLine();
+
+      switch (day) {
+
+        case "Pazartesi":
+        case "Sali":
+        case "Carsamba":
+        case "Persembe":
+        case "Cuma":
+
+          System.out.println("Calisma gunu ");
+        break;
+
+        case "Cumartesi":
+        case "Pazar":
+
+          System.out.println("Tatil ");
+        break;
+
+        default:
+          System.out.println("Girilen gun hatali ");
+        break;
+
+      }
+
+    }
+
+  }
