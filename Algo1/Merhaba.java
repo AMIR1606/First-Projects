2c9e369878b38325c0a21a9ffc27874cc4d91a21
AMIR1606 <amersheikhhader@gmail.com>
2024-11-03 16:20:20 +0300

    Algo1

---
+++ Algo1/Merhaba.java
@@ -0,0 +1,11 @@
+public class Merhaba {
+
+  public static void main(String[] args){
+
+    //Konsol penceresinde selamlama
+
+      System.out.println("Merhaba, Dunya!");
+
+  }
+    
+}
--- github.java
+++ github.java
@@ -1,8 +1,13 @@
 public class github {
   public static void main(String[] args) {
-    System.out.println("Hello, GitHub!");
-    System.out.println(" My name is amir");
-  
+      System.out.println("Hello, GitHub!");
+    
+    if (100%5==0)
+      System.out.println("CCCCCCCCCCC");
+    
+    else 
+      System.out.println("BBBBBBBBBB");
+      
   }
 
 }
