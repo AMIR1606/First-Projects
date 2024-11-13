31e18748da7318d66c10283c268633cdf2a04fd9
AMIR1606 <amersheikhhader@gmail.com>
2024-11-13 20:11:18 +0300

    Upload

---
+++ Algo Lab1/Sorulab2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Sorulab2 {
+    public static void main (String[] args) {
+        int n = 0,aralik, i;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lutfen makine sayisini griniz: ");
+        int MS = input.nextInt();
+        System.out.println("Lutfen ay numarasini giriniz: ");
+        int AY = input.nextInt();
+
+        if (AY > 2 && AY < 9 ){
+            i = 480;
+            aralik = 45;
+        } else {
+            i = 540;
+            aralik = 30;
+        }
+        for (; MS > 0; MS--){
+            System.out.print(i+", ");
+            i = i - aralik;
+        }
+    }
+}
