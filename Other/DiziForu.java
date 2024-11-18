9d1ddbc8a172fc7a440c06edae25a4ef7f0f8514
AMIR1606 <amersheikhhader@gmail.com>
2024-11-18 21:00:41 +0300

    Upload

---
+++ Other/DiziForu.java
@@ -0,0 +1,8 @@
+public class DiziForu {
+    public static void main(String[] args) {
+        int dizi[] = {12,15,19,17};
+        for(int i:dizi){
+            System.out.println(i);
+        }
+    }
+}
---
+++ Other/Faktoriyel.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Faktoriyel{
+    public static void main(String[] args) {
+        int faktoriyel = 1;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Faktoryeli hesaplanacak sayiyi giiniz: ");
+        int num = input.nextInt();
+        for(;num > 0; num--){
+            faktoriyel = faktoriyel * num;
+        }
+        if(num < 0){
+            System.out.println("0'dan kucuk olan sayilarin faktoriyeli yok.");
+        }else {
+            System.out.println("Sayin faktoriyeli = " + faktoriyel);
+        }
+    }
+}
