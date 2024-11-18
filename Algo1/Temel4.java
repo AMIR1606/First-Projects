114ebec1be06a195f8c4fd32a65a4ec7ab31d222
AMIR1606 <amersheikhhader@gmail.com>
2024-11-18 22:44:52 +0300

    edit

--- Algo1/Temel4.java
+++
@@ -1,9 +0,0 @@
-public class Temel4 {
-    public static void main(String[] args){
-        //Overflow
-        int a = 150;
-        byte b = (byte)a;
-        System.out.println(a);
-        System.out.println(b);
-    }
-}
