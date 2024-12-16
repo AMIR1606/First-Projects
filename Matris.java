e20890a0d010dcb608a5f93a8ee4b903537fa459
AMIR1606 <amersheikhhader@gmail.com>
2024-12-16 10:20:11 +0300

    upload

--- Matris.java
+++ Matris.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
-public class tttt {
+public class Matris {
     public static void main(String []args) {
-        int y = 2;
+
         Scanner input = new Scanner(System.in);
-        System.out.println("4 ile 8 arasinda bir sayi giriniz: ");
+        System.out.println(" Matrisin satir sayisi giriniz: ");
         int sayi = input.nextInt();
 
         for (int i = 0; i < sayi; i++){
-            int x = 10;
+            int x = 10, y = 2;
             x = x*(i+1);
             System.out.println("\n");
+            
             for (int j = 0; j < 4; j++){
                 System.out.print(x + "\t");
                 x -= y;
